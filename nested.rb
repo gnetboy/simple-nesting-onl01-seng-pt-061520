486895d24876332dd2ce919ad8a44be47b75b0cf
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-06-26 05:34:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages].to_s.split('\ ')
+     programmer_hash[:dennis_ritchie][:languages].to_s.join("")
 end
 
 def adding_matz
